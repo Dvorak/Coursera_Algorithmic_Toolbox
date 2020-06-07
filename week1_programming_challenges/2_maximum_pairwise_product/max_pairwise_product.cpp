a3d0f5f54d1c42c631889a8e2ab4864310db0add
nuc_ubuntu <zhiyuan_zhang92@hotmail.com>
2020-06-07 23:46:34 +0200

    v_0 without stress test

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -2,23 +2,23 @@
 #include <vector>
 #include <algorithm>
 
-int MaxPairwiseProduct(const std::vector<int>& numbers) {
-    int max_product = 0;
+long long MaxPairwiseProduct(const std::vector<int>& numbers) {
+    long long max_product = 0;
     int n = numbers.size();
 
     int max_index_1 = -1;
-    int max_index_2 = -1
+    int max_index_2 = -1;
     for (int i = 0; i < n; i++){
-      if (number[i] > number[max_index_1]){
+      if (numbers[i] > numbers[max_index_1])
         max_index_1 = i;
     }
 
     for (int j = 0; j < n; j++){
-    	if((j != max_index_1) && (number[j] > number[max_index_2]))
+    	if((j!= max_index_1) && (numbers[j] > numbers[max_index_2]))
 		    max_index_2 = j;
     }
 
-    max_product = number[max_index_1] * number[max_index_2];
+    max_product = ((long long)(numbers[max_index_1])) * numbers[max_index_2];
 
     return max_product;
 }
@@ -31,6 +31,6 @@ int main() {
         std::cin >> numbers[i];
     }
 
-    std::cout << MaxPairwiseProduct(numbers); << "\n";
+    std::cout << MaxPairwiseProduct(numbers) << "\n";
     return 0;
 }
